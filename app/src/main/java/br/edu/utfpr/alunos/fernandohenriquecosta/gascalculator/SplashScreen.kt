65e6bb41254d19567fc4eb834d033b91955f8f47
Fernando Costa <fernando.costa@itriad.org.br>
2024-07-07 22:01:41 -0400

    Add last files

---
+++ app/src/main/java/br/edu/utfpr/alunos/fernandohenriquecosta/gascalculator/SplashScreen.kt
@@ -0,0 +1,19 @@
+package br.edu.utfpr.alunos.fernandohenriquecosta.gascalculator
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+}
